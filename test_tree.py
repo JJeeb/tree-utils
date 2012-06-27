f52be4611881d80ced275815d67dd875c8575e99
Jean-Baptiste Potonnier <jean.baptiste.potonnier@gmail.com>
2012-06-27 21:52:19 +0200

    test class

--- test_tree.py
+++ test_tree.py
@@ -10,75 +10,75 @@ TECHNOLOGIES = {
          'children': [ {'name': 'Mac'}, {'name': 'Mobile'}]}]
     }
 
+class TestTree:
 
-def test_enumerate_paths():
-    assert_equal([
-        (['Technology'], 'Technology'),
-        (['Technology', 'Programming'], 'Programming'),
-        (['Technology', 'Programming', 'Python'], 'Python'),
-        (['Technology', 'Programming', 'Ruby'], 'Ruby'),
-        (['Technology', 'Enterprise'], 'Enterprise'),
-        (['Technology', 'Enterprise', 'Mac'], 'Mac'),
-        (['Technology', 'Enterprise', 'Mobile'], 'Mobile')],
-        list((path, node['name']) for path, node in enumerate_paths(TECHNOLOGIES)))
+    def test_enumerate_paths(self):
+        assert_equal([
+            (['Technology'], 'Technology'),
+            (['Technology', 'Programming'], 'Programming'),
+            (['Technology', 'Programming', 'Python'], 'Python'),
+            (['Technology', 'Programming', 'Ruby'], 'Ruby'),
+            (['Technology', 'Enterprise'], 'Enterprise'),
+            (['Technology', 'Enterprise', 'Mac'], 'Mac'),
+            (['Technology', 'Enterprise', 'Mobile'], 'Mobile')],
+            list((path, node['name']) for path, node in enumerate_paths(TECHNOLOGIES)))
 
 
-def test_find_by_path():
-   assert_equal({'name': 'Python'},
-        find_by_path(TECHNOLOGIES, ['Technology', 'Programming', 'Python']))
-  
-def test_find_by_path_when_not_found(): 
-   assert_equal(None,
-        find_by_path(TECHNOLOGIES, ['Technology', 'does_not_exist']))
+    def test_find_by_path(self):
+       assert_equal({'name': 'Python'},
+            find_by_path(TECHNOLOGIES, ['Technology', 'Programming', 'Python']))
 
-def test_iter_tree():
-    assert_equal([{'children': [{'children': [{'name': 'Python'}, {'name': 'Ruby'}],
-                   'name': 'Programming'},
-                  {'children': [{'name': 'Mac'}, {'name': 'Mobile'}],
-                   'name': 'Enterprise'}],
-                 'name': 'Technology'},
-            {'children': [{'name': 'Python'}, {'name': 'Ruby'}], 'name': 'Programming'},
-            {'name': 'Python'},
-            {'name': 'Ruby'},
-            {'children': [{'name': 'Mac'}, {'name': 'Mobile'}], 'name': 'Enterprise'},
-            {'name': 'Mac'},
-            {'name': 'Mobile'}],
-    list(tree(TECHNOLOGIES)))
+    def test_find_by_path_when_not_found(self):
+       assert_equal(None,
+            find_by_path(TECHNOLOGIES, ['Technology', 'does_not_exist']))
+
+    def test_iter_tree(self):
+        assert_equal([{'children': [{'children': [{'name': 'Python'}, {'name': 'Ruby'}],
+                       'name': 'Programming'},
+                      {'children': [{'name': 'Mac'}, {'name': 'Mobile'}],
+                       'name': 'Enterprise'}],
+                     'name': 'Technology'},
+                {'children': [{'name': 'Python'}, {'name': 'Ruby'}], 'name': 'Programming'},
+                {'name': 'Python'},
+                {'name': 'Ruby'},
+                {'children': [{'name': 'Mac'}, {'name': 'Mobile'}], 'name': 'Enterprise'},
+                {'name': 'Mac'},
+                {'name': 'Mobile'}],
+        list(tree(TECHNOLOGIES)))
+
+    def test_generator_expression(self):
+        assert_equal(['Technology', 'Programming', 'Python', 'Ruby',
+                    'Enterprise', 'Mac', 'Mobile'],
+            list(e['name'] for e in tree(TECHNOLOGIES)))
+
+        assert_equal(['Python', 'Ruby', 'Mac', 'Mobile'],
+            list(e['name'] for e in tree(TECHNOLOGIES) if is_leaf(e)))
+
+    def test_update_with_side_effect(self):
+        technologies = {
+            'name': 'Technology',
+            'children': [
+            {
+                'name': 'Programming',
+                'children': [
+                {'name': 'Python'},
+                {'name': 'Ruby'}
+                ]
+            }]}
+
+        python = (e for e in tree(technologies) if e['name'] == 'Python').next()
+        python.update(tag='cool')
+
+        assert_equal({
+            'name': 'Technology',
+            'children': [
+            {
+                'name': 'Programming',
+                'children': [
+                {'name': 'Python', 'tag': 'cool'},
+                {'name': 'Ruby'}
+                ] }]},
+        technologies)
 
 def is_leaf(e):
     return 'children' not in e
-
-def test_generator_expression():
-    assert_equal(['Technology', 'Programming', 'Python', 'Ruby', 
-                'Enterprise', 'Mac', 'Mobile'], 
-        list(e['name'] for e in tree(TECHNOLOGIES)))
-
-    assert_equal(['Python', 'Ruby', 'Mac', 'Mobile'], 
-        list(e['name'] for e in tree(TECHNOLOGIES) if is_leaf(e)))
-
-def test_update_with_side_effect():
-    technologies = { 
-        'name': 'Technology',
-        'children': [
-        {
-            'name': 'Programming',
-            'children': [
-            {'name': 'Python'},
-            {'name': 'Ruby'}
-            ]
-        }]} 
-    
-    python = (e for e in tree(technologies) if e['name'] == 'Python').next()
-    python.update(tag='cool')
-
-    assert_equal({ 
-        'name': 'Technology',
-        'children': [
-        {
-            'name': 'Programming',
-            'children': [
-            {'name': 'Python', 'tag': 'cool'},
-            {'name': 'Ruby'}
-            ] }]}, 
-    technologies) 
-    
